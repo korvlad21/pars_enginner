3405db8126259ec1b79f7c37cad1278ebbbb14db
Maximilyanus29 <vladek2000@inbox.ru>
2022-09-16 15:52:31 +0300

    Начал первый сайт

--- app/Models/Pars.php
+++ app/Models/Pars.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Orchestra\Parser\Xml\Facade as XmlParser;
@@ -11,9 +12,20 @@ class Pars extends Model
     public function Pars1()
     {
         $xml = XmlParser::load('https://service.ferroli.ru/storage/catalogs/xml-cml.xml')->getContent();
-
-        $xml=$xml->Каталог->Товары;
-        dd($xml);
+        $characts=$xml->Классификатор->свойства->Свойство;
+        $i=0;
+        foreach ($characts as $charact)
+        {
+            $i++;
+            if($i==2)
+            {
+                dd($charact);
+            }
+        }
+        dd($characts[1]->Ид[0]);
+        $products=$xml->Каталог->Товары->Товар;
+//        dd((string)$products[2]->Ид[0]);
+        dd($products[2]);
 
     }
 }
