35944fdd391cc844447436cba052b92f5e81974e
Maximilyanus29 <vladek2000@inbox.ru>
2022-09-21 16:05:12 +0300

    Парсинг 8-й файлы и фотки получаю

--- app/Models/Pars.php
+++ app/Models/Pars.php
@@ -242,21 +242,33 @@ class Pars extends Model
                 $doc = phpQuery::newDocument($String);
                 $entry = $doc->find('h1');
                 $data['name']=pq($entry)->text();
-                $entry = $doc->find('div.detail_text');
-                $description = iconv("windows-1251", "UTF-8", pq($entry)->html());
-                $data['description'] = preg_replace('/[\t\n]+/', '', $description);
-                $entry = $doc->find('div.prices_block div.price span.price_value');
-                $data['price'] = preg_replace("/[^,.0-9]/", '', pq($entry)->text());
-                if($data['price']=='')
-                {
-                    $data['price']=0;
+//                $entry = $doc->find('div.detail_text');
+//                $description = iconv("windows-1251", "UTF-8", pq($entry)->html());
+//                $data['description'] = preg_replace('/[\t\n]+/', '', $description);
+//                $entry = $doc->find('div.prices_block div.price span.price_value');
+//                $data['price'] = preg_replace("/[^,.0-9]/", '', pq($entry)->text());
+//                if($data['price']=='')
+//                {
+//                    $data['price']=0;
+//                }
+                $entry = $doc->find('div.inner a');
+                foreach ($entry as $row) {
+                    $img = pq($row)->attr('href');
                 }
-                $good = Good_p::create($data);
-                $entry = $doc->find('div.description a');
+
+//                $url = "https://www.logikamarket.ru" . $img;
+//                $file_extension = pathinfo($url)['extension'];
+//                $file_name = 'good/' . Str::random(30) . '.' . $file_extension;
+//                $file = file_get_contents($url);
+//                Storage::disk('public')->put($file_name, $file);
+//                $data['image'] = str_replace('http://localhost', '', Storage::disk('public')->url($file_name));
+//                $good = Good_p::create($data);
+                $entry = $doc->find('div.wraps div.row div.col-md-6 a');
                 foreach ($entry as $row)
                 {
                     $filehref=pq($row)->attr('href');
-                    $url = "https://ekoport.ru" . $filehref;
+                    dd($filehref);
+                    $url = "https://www.logikamarket.ru" . $filehref;
                     $file_extension = pathinfo($url)['extension'];
                     $file_name = 'files/' . Str::random(30) . '.' . $file_extension;
                     $file = file_get_contents($url);
